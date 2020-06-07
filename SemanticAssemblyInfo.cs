d6e482c368020766a3d2128949746c8e133fe08e
Bryan Apellanes <bryan@threeheadz.com>
2020-06-07 11:36:03 -0700

    build scripts and workflow updates

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.2416083107")]
-[assembly: AssemblyCommit("f188137")]
-[assembly: AssemblySemanticVersion("1.0.5-test+f188137")]
+[assembly: AssemblyFileVersion("1.0.5.473139403")]
+[assembly: AssemblyCommit("b02837c")]
+[assembly: AssemblySemanticVersion("1.0.5-test+b02837c")]
