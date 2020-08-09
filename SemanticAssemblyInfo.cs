0cac9bbc690fd96f22ab7b22adf686ad1c76fa56
Bryan Apellanes <bryan@threeheadz.com>
2020-08-09 09:47:42 -0700

    push script

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.2164544070")]
-[assembly: AssemblyCommit("4f9bd78")]
-[assembly: AssemblySemanticVersion("1.0.5-test+4f9bd78")]
+[assembly: AssemblyFileVersion("1.0.5.2971323663")]
+[assembly: AssemblyCommit("544610b")]
+[assembly: AssemblySemanticVersion("1.0.5-test+544610b")]
