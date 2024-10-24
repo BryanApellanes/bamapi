7746f5601d8cf0d921dab2a7c3960834a808fc48
Bryan Apellanes <bryan.apellanes@gmail.com>
2024-10-24 08:31:37 -0500

    wip

--- bamapi/SemanticAssemblyInfo.cs
+++ bamapi/SemanticAssemblyInfo.cs
@@ -1,9 +1,10 @@
 using System.Reflection;
+using Bam;
 using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.6.0")]
 [assembly: AssemblyFileVersion("1.0.6.0")]
 [assembly: AssemblyCommit("18ea5b7")]
-[assembly: AssemblySemanticVersion("1.0.6-18ea5b7")]
+//[assembly: AssemblySemanticVersion("1.0.6-18ea5b7")]
 [assembly: AssemblyDescription("SemanticVersion=1.0.6-18ea5b7, Revision=1.0.6.552749054")]
 [assembly: AssemblyProduct("bamapi")]
